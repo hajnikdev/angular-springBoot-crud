8906c550272484e6c42403b1bd2612206fb34e65
Maroš Hajník <hajnik.work@gmail.com>
2021-04-26 09:34:03 +0200

    Completed service class and implemented exception handling

--- Backend/employeemanager/src/main/java/com/getarrays/employeemanager/repo/EmployeeRepo.java
+++ Backend/employeemanager/src/main/java/com/getarrays/employeemanager/repo/EmployeeRepo.java
@@ -3,5 +3,10 @@ package com.getarrays.employeemanager.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.getarrays.employeemanager.model.Employee;
 
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
 }
