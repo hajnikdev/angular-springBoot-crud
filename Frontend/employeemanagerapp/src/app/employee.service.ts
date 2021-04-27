4c7c1b3662a274626c2c9fce855d9745fa4913b9
Maroš Hajník <hajnik.work@gmail.com>
2021-04-27 08:39:57 +0200

    Created ui for employees manager application

--- Frontend/employeemanagerapp/src/app/employee.service.ts
+++ Frontend/employeemanagerapp/src/app/employee.service.ts
@@ -1,37 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-  private apiServerUrl = '';
-
   constructor(private http: HttpClient) {}
 
   public getEmployees(): Observable<Employee[]> {
-    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+    return this.http.get<Employee[]>(`${environment.apiBaseUrl}/employee/all`);
   }
 
   public addEmployee(employee: Employee): Observable<Employee> {
     return this.http.post<Employee>(
-      `${this.apiServerUrl}/employee/add`,
+      `${environment.apiBaseUrl}/employee/add`,
       employee
     );
   }
 
   public updateEmployee(employee: Employee): Observable<Employee> {
     return this.http.put<Employee>(
-      `${this.apiServerUrl}/employee/update`,
+      `${environment.apiBaseUrl}/employee/update`,
       employee
     );
   }
 
   public deleteEmployee(employeeId: number): Observable<void> {
     return this.http.delete<void>(
-      `${this.apiServerUrl}/employee/delete/${employeeId}`
+      `${environment.apiBaseUrl}/employee/delete/${employeeId}`
     );
   }
 }
